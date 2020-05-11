12f1078f935d2fe0f8d6af52ac68f1fb31915c83
Dumitru Chiriac <56750709+dit555@users.noreply.github.com>
2020-05-10 17:51:02 -0700

    Update dchir002_lab5_part1.c

--- turnin/dchir002_lab5_part1.c
+++ turnin/dchir002_lab5_part1.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	demo: https://youtu.be/6rwPabz4ouI
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
